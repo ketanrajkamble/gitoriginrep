ebe7372cffa8c581e3295788820b3f77ac5130ce
Surender Singh Rathore <surenbika@gmail.com>
2018-03-22 11:06:28 +0530

    The First Commit to learn GIT

---
+++ src/main/java/org/learngit/App.java
@@ -0,0 +1,13 @@
+package org.learngit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
